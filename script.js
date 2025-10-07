da81a9ba481bc1548cfd072a489872ad4a7607be
justynize <nwobajusticeekene12@gamil.com>
2025-10-07 22:57:41 +0100

    added new icons

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const apiurl = "https://api.openweathermap.org/data/2.5/weather?=&units=metric&q
 
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
-// const weatherIcon = document.querySelector(".weather-icon");
+const weatherIcon = document.querySelector(".weather-icon");
 
 async function checkweather(city) {
     const response = await fetch(apiurl + city + `&appid=${apikey}`);
@@ -16,17 +16,17 @@ async function checkweather(city) {
     document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
     document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
 
-    // if (data.weather[0].main == "Clouds") {
-    //     weatherIcon.src = "/images/clouds.png";
-    // } else if (data.weather[0].main == "Clear") {
-    //     weatherIcon.src = "Assets/clear-png.webp";
-    // } else if (data.weather[0].main == "Rain") {
-    //     weatherIcon.src = "Assets/img.rain.png";
-    // } else if (data.weather[0].main == "Drizzle") {
-    //     weatherIcon.src = "Assets/img.drizzle.webp";
-    // } else if (data.weather[0].main == "Mist") {
-    //     weatherIcon.src = "Assets/img.misty.webp";
-    // }
+    if (data.weather[0].main == "Clouds") {
+        weatherIcon.src = "/images/clouds.png";
+    } else if (data.weather[0].main == "Clear") {
+        weatherIcon.src = "Assets/clear-png.webp";
+    } else if (data.weather[0].main == "Rain") {
+        weatherIcon.src = "Assets/img.rain.png";
+    } else if (data.weather[0].main == "Drizzle") {
+        weatherIcon.src = "Assets/img.drizzle.webp";
+    } else if (data.weather[0].main == "Mist") {
+        weatherIcon.src = "Assets/img.misty.webp";
+    }
 
 }
 
